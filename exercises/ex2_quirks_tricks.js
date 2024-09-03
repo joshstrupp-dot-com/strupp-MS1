ef213dfcd6c093195b8ecde15c1062e0d5dca5ba
Josh Strupp <struj580@newschool.edu>
2024-09-03 14:42:23 -0400

    complete ex 3

--- exercises/ex2_quirks_tricks.js
+++ exercises/ex2_quirks_tricks.js
@@ -32,23 +32,23 @@ console.log(test3);
 
 
 
-// // Task
-// // Change this code so test4 is false and test5 is true. Use console.log() to confirm your cod works.
+// Task
+// Change this code so test4 is false and test5 is true. Use console.log() to confirm your cod works.
 
-// var test4 = 0 == "";
-// var test5 = 1 === "1";
+var test4 = 0 == "";
+var test5 = 1 === "1";
 
-// console.log("test4 is", test4, "and test 5 is", test5);
+console.log("test4 is", test4, "and test 5 is", test5);
 
-// // Task
-// // What are the values of p, q, and r? Research what is going on here.
-// var w = 0.1;
-// var x = 0.2;
-// var y = 0.4;
-// var z = 0.5;
+// Task
+// What are the values of p, q, and r? Research what is going on here.
+var w = 0.1;
+var x = 0.2;
+var y = 0.4;
+var z = 0.5;
 
-// var p = w + x;
+var p = w + x;
 
-// var q = z - x;
+var q = z - x;
 
-// var r = y - w;
+var r = y - w;
--- exercises/ex3_dom_manipulation.js
+++ exercises/ex3_dom_manipulation.js
@@ -5,6 +5,7 @@
 
 // Task
 // What does DOM stand for?
+// ANSWER: Document Object Model
 
 // Task
 // Open the file index.html in your browser. Open the index.html file in VS Code, right-click the tab and select "Open in Browser"
@@ -15,21 +16,26 @@
 
 // Task
 // What does the following code do?
+
 const viz = document.body.querySelector(".viz");
 const button = document.body.querySelector("#button");
+// creates two variable, viz and button, that select from HTML elements with class viz and elements with ID button
 
 console.log(viz, viz.children);
 
-const addChildToViz = () => {
+const addChildToViz = (len) => {
   const newChild = document.createElement("div");
   newChild.className = "rectangle";
-  newChild.style.height = Math.random() * 100 + "px";
+  newChild.style.height = len * 10 + "px";
   viz.appendChild(newChild);
 };
+// creates a function that creates a new empty div as "newChild" with class rectangle and random height, then appends it to the viz element
+
 
 // Task
 // Modify index.html to make this event listener work
 button.addEventListener("click", addChildToViz);
+// adds an event listener to the button element that listens for a click and runs the addChildToViz function
 
 // Task
 // Where can you see the results of the console.log below? How is it different from in previous exercises?
@@ -39,9 +45,12 @@ function drawIrisData() {
     .fetch("./iris_json.json")
     .then(data => data.json())
     .then(data => {
-      console.log(data);
+      data.forEach(e => {
+        addChildToViz(e.petallength);
+      });
     });
 }
+// fetches the iris_json.json file, grabs only JSON, then runs addChildToViz function on each element in the JSON file to add a rectangle representing each petal length in the DOM
 
 drawIrisData();
 
